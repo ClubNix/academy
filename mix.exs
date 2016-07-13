454777620edf327e987b37f07c1b56869643c6a7
Minijackson <minijackson@riseup.net>
2016-07-13 02:57:38 +0200

    [Travis] Adding elixirc test path when MIX_ENV=travis

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule Academy.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
+  defp elixirc_paths(:travis), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
   # Specifies your project dependencies.
