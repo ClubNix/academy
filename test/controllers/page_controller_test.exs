5b63e4a4b9daa875e024e8d9f334f41bf5becc69
Minijackson <minijackson@riseup.net>
2016-07-15 01:02:51 +0200

    Making dummy PageController test pass

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule Academy.PageControllerTest do
   use Academy.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
+    conn = get conn, "/about"
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
