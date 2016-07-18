33f82b1c20ca3fd9c43dd5e6502579910c415c17
Minijackson <minijackson@riseup.net>
2016-07-18 23:00:16 +0200

    Stylize errors. See issue #4

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -6,23 +6,6 @@ defmodule Academy.ErrorView do
   use Academy.Web, :view
 
   @doc ~S"""
-  Render an error.
-  """
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
-
-  def render("500.html", _assigns) do
-    "Server internal error"
-  end
-
-  def render("401.html", _assigns) do
-    "Unauthorized"
-  end
-
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
-  @doc ~S"""
   Function called when a template isn't found
   """
   def template_not_found(_template, assigns) do
